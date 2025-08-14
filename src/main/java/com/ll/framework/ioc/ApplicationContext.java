313b3854ed4833e0225d74ecdab6592b8428f03d
Yonghyun Cho <yonghyun.cho@outlook.com>
2025-08-14 17:10:32 +0900

    [feat] 스프링부트 IoC 컨테이너 클론 (v1) - 조용현

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,27 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
+public class ApplicationContext {
+    TestPostService testPostService;
+    TestPostRepository testPostRepository;
+    TestFacadePostService testFacadePostService;
+
+    public ApplicationContext() {
+        testPostRepository = new TestPostRepository();
+        testPostService = new TestPostService(testPostRepository);
+        testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
-    }
+        switch (beanName) {
+            case "testPostService" -> {return (T)testPostService;}
+            case "testPostRepository" -> {return (T)testPostRepository;}
+            case "testFacadePostService" -> {return (T)testFacadePostService;}
+            default -> {return (T)null;}
+        }
+    };
 }
+
